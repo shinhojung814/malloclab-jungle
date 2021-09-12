16a5ac2127945837e79bedb940bb67c5da514fb2
Shinho Jung <87798782+shinhojung814@users.noreply.github.com>
2021-09-12 22:08:19 +0900

    Update mm.h

--- mm.h
+++ mm.h
@@ -1,44 +1,45 @@
 #include <stdio.h>
 
-extern int mm_init (void);
-extern void *mm_malloc (size_t size);
-extern void mm_free (void *bp);
+extern int mm_init(void);
+extern void *mm_malloc(size_t size);
+extern void mm_free(void *bp);
 extern void *mm_realloc(void *ptr, size_t size);
 
 /* Word and header/footer size (bytes) */
-#define WSIZE       4
+#define WSIZE 4
 /* Double word size (bytes) */
-#define DSIZE       8
+#define DSIZE 8
 /* Extend heap by this amount (bytes) */
-#define CHUNKSIZE   (1 << 12)
+#define CHUNKSIZE (1 << 12)
 
-#define MAX(x, y)   ((x) > (y) ? (x) : (y))
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
 
 /* Pack a size and allocated bit into a word */
-#define PACK(size, alloc)   ((size) | (alloc))
+#define PACK(size, alloc) ((size) | (alloc))
 
 /* Read and write a word at address p */
-#define GET(p)      (*(unsigned int *)(p))
+#define GET(p) (*(unsigned int *)(p))
 #define PUT(p, val) (*(unsigned int *)(p) = (val))
 
 /* Read the size and allocated fields from address p */
-#define GET_SIZE(p)     (GET(p) & ~0x7)
-#define GET_ALLOC(p)    (GET(p) & 0x1)
+#define GET_SIZE(p) (GET(p) & ~0x7)
+#define GET_ALLOC(p) (GET(p) & 0x1)
 
 /* Given block ptr bp, compute address of its header and footer */
-#define HDRP(pb)    ((char *)(bp)-WSIZE)
-#define FTRP(bp)    ((char *)(bp) + GET_SIZE(HDRP(bp) - DSIZE))
+#define HDRP(bp) ((char *)(bp)-WSIZE)
+#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp) - DSIZE))
 
 /* Given block ptr pb, compute address of next and previous blocks */
-#define NEXT_BLKP(bp)   ((char *)(bp) + GET_SIZE(((char *)(bp)-WSIZE)))
-#define PREV_BLKP(bp)   ((char *)(bp)-GET_SIZE(((char *)(bp)-DSIZE)))
+#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp)-WSIZE)))
+#define PREV_BLKP(bp) ((char *)(bp)-GET_SIZE(((char *)(bp)-DSIZE)))
 
 /* 
  * Students work in teams of one or two.  Teams enter their team name, 
  * personal names and login IDs in a struct of this
  * type in their bits.c file.
  */
-typedef struct {
+typedef struct
+{
     char *teamname; /* ID1+ID2 or ID1 */
     char *name1;    /* full name of first member */
     char *id1;      /* login ID of first member */
